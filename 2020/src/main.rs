602efabd5e8131723089f9bf76973a3daca1f5ff
Caleb Quilley <c@cquilley.co.uk>
2020-12-20 17:39:26 +0000

    2020.17,18

--- 2020/src/main.rs
+++ 2020/src/main.rs
@@ -1,15 +1,21 @@
 #![feature(str_split_once)]
-#![allow(dead_code)]
+#![feature(const_generics)]
+#![feature(split_inclusive)]
+#![feature(array_chunks)]
+#![allow(dead_code, incomplete_features)]
 
 use std::io::{self, BufRead, BufReader};
 use std::fs;
 use std::path::Path;
 use std::collections::{HashMap, HashSet};
 use std::str::FromStr;
+use std::mem;
 
 fn main() {
-    day15();
-    day16();
+    //day15();
+    //day16();
+    //day17();
+    day18();
 }
 
 fn day15() {
@@ -27,11 +33,11 @@ fn day15() {
             .unwrap_or(0);
 
         if i == 2019 {
-            println!("p1: {}", latest);
+            println!("15.1: {}", latest);
         }
     }
 
-    println!("p2: {}", latest);
+    println!("15.2: {}", latest);
 }
 
 fn file<P: AsRef<Path>, S, T>(x: P, state: &mut S, cb: fn(&str, &mut S) -> T) -> Result<Vec<T>, io::Error> {
@@ -93,7 +99,7 @@ fn day16() {
             }
         }
     }
-    println!("p1: {}", rate);
+    println!("16.1: {}", rate);
 
     let mut departure: u64 = 1;
     let mut constraints = Vec::new();
@@ -129,5 +135,185 @@ fn day16() {
         }
     }
 
-    println!("p2: {}", departure);
+    println!("16.2: {}", departure);
+}
+
+fn day17() {
+    struct Neighbours<const N: usize> {
+        base: [i64; N],
+        counter: usize,
+    }
+
+    impl<const N: usize> Neighbours<N> {
+        fn new(base: [i64; N]) -> Neighbours<N> {
+            Neighbours { base, counter: 0 }
+        }
+    }
+
+    impl<const N: usize> Iterator for Neighbours<N> {
+        type Item = [i64; N];
+        fn next(&mut self) -> Option<[i64; N]> {
+            if self.counter >= 3usize.pow(N as u32) {
+                return None;
+            }
+            let mut place = self.counter as i64;
+            let mut offset = self.base.clone();
+
+            for i in 0..N {
+                offset[i] += (place % 3) - 1;
+                place /= 3;
+            }
+
+            self.counter += 1;
+            Some(offset)
+        }
+    }
+
+    fn step<const N: usize>(alive: &mut HashSet<[i64; N]>,
+                            output: &mut HashSet<[i64; N]>,
+                            considered: &mut HashMap<[i64; N], usize>) -> usize {
+        output.clear();
+        considered.clear();
+        for coord in alive.iter() {
+            for neighbour in Neighbours::new(*coord) {
+                considered.entry(neighbour)
+                    .and_modify(|x| *x += 1)
+                    .or_insert(1);
+            }
+        }
+
+        for (coord, count) in considered.drain() {
+            if count == 3 || (alive.contains(&coord) && count == 4) {
+                output.insert(coord.clone());
+            }
+        }
+
+        output.len()
+    }
+
+    fn parse(s: &str, _: &mut ()) -> Vec<usize> {
+        s.chars()
+            .enumerate()
+            .filter_map(|(i, c)| if c == '#' { Some(i) } else { None })
+            .collect()
+    }
+
+    let mut alive = HashSet::new();
+    let mut output = HashSet::new();
+    let mut considered = HashMap::new();
+
+    for (x, row) in file("17.input", &mut (), parse).unwrap().into_iter().enumerate() {
+        for y in row {
+            alive.insert([x as i64, y as i64, 0, 0]);
+        }
+    }
+
+    for _ in 0..=6 {
+        step(&mut alive, &mut output, &mut considered);
+        mem::swap(&mut alive, &mut output);
+    }
+
+    println!("17: {}", output.len());
+}
+
+fn day18() {
+    #[derive(Debug, PartialEq, Eq, Copy, Clone)]
+    enum Token {
+        Num(i64),
+        Op(char),
+    }
+
+    fn lex(mut s: &str) -> (Vec<Token>, Vec<usize>) {
+        let mut tokens = vec!();
+        let mut depths = vec!();
+        let mut depth = 0;
+        while !s.is_empty() {
+            let c = s.chars().next().unwrap();
+            if "*+()".contains(c) {
+                s = &s[1..].trim();
+                match c {
+                    c if c == '+' || c == '*' => {
+                        tokens.push(Token::Op(c));
+                        depths.push(depth);
+                    }
+                    '(' => depth += 1,
+                    ')' => depth -= 1,
+                    _ => panic!("bad char match"),
+                }
+            }
+            let cut = s.find(|c: char| !c.is_digit(10)).unwrap_or(s.len());
+            if cut > 0 {
+                tokens.push(Token::Num(i64::from_str(&s[..cut]).unwrap()));
+                depths.push(depth);
+                s = s[cut..].trim();
+            }
+        }
+
+        (tokens, depths)
+    }
+
+    fn eval((mut tokens, mut depths): (Vec<Token>, Vec<usize>), eval_expr: fn(&[Token]) -> i64) -> i64 {
+        if tokens.is_empty() {
+            return 0;
+        }
+
+        loop {
+            if tokens.len() == 1 {
+                break;
+            }
+            let max_depth = depths.iter().copied().max().unwrap();
+            let start = depths.iter().position(|x| *x == max_depth).unwrap();
+            let end = start + depths.iter().skip(start).position(|x| *x != max_depth).unwrap_or(depths.len() - start);
+            let result = eval_expr(&tokens[start..end]);
+            tokens.drain(start..end).for_each(mem::drop);
+            depths.drain(start..end).for_each(mem::drop);
+            tokens.insert(start, Token::Num(result));
+            depths.insert(start, if max_depth == 0 { 0 } else { max_depth - 1 })
+        }
+
+        match tokens.pop() {
+            Some(Token::Num(x)) => x,
+            x => panic!("Unexpected result: {:?}", x)
+        }
+    }
+
+    fn evaluate_equally(tokens: &[Token]) -> i64 {
+        let mut iter = tokens.iter();
+        let mut acc = match iter.next() {
+            Some(Token::Num(x)) => *x,
+            t => panic!("Unexpected subexpr prefix: {:?}", t),
+        };
+        loop {
+            match (iter.next(), iter.next()) {
+                (Some(Token::Op('+')), Some(Token::Num(x))) => acc += x,
+                (Some(Token::Op('*')), Some(Token::Num(x))) => acc *= x,
+                (None, None) => return acc,
+                (x, y) => panic!("Bad subexpr: {} {:?} {:?}", acc, x, y)
+            }
+        }
+    }
+
+    fn evaluate_with_precedence(tokens: &[Token]) -> i64 {
+        let mut iter = tokens.iter();
+        let mut sums = vec!(match iter.next() {
+            Some(Token::Num(x)) => *x,
+            t => panic!("Unexpected subexpr prefix: {:?}", t),
+        });
+
+        loop {
+            match (iter.next(), iter.next()) {
+                (Some(Token::Op('+')), Some(Token::Num(x))) => sums.last_mut().map(|acc| *acc += *x).unwrap_or(()),
+                (Some(Token::Op('*')), Some(Token::Num(x))) => sums.push(*x),
+                (None, None) => return sums.iter().product(),
+                (x, y) => panic!("Bad subexpr {:?} {:?} {:?}", sums, x, y)
+            }
+        }
+    }
+
+    let mut total = 0;
+    file("18.input", &mut total, |s, x| *x += eval(lex(s), evaluate_equally)).unwrap();
+    println!("18.1: {}", total);
+    total = 0;
+    file("18.input", &mut total, |s, x| *x += eval(lex(s), evaluate_with_precedence)).unwrap();
+    println!("18.2: {}", total);
 }
